76a19f96d4941e97ed4c2b0b7a77b856d508cef3
AndrewBlanchard <152227162+AndrewBlanchard@users.noreply.github.com>
2024-01-26 12:54:42 -0600

    Update script.js
    Updated CSS to add leaderboard container

--- script.js
+++ script.js
@@ -1,220 +1,228 @@
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const choiceD = document.getElementById("D");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreContainer");
+body{
+    font-size: 20px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+}
 
-// create our questions
-let questions = [
-    {
-        question: "Which Pokemon is a starter from the Johto region?",
-        imgSrc: "Assets/PNGs/Cyndaquil_Shadow.png",
-        choiceA: "Turtwig",
-        choiceB: "Charmander",
-        choiceC: "Cyndaquil",
-        choiceD: "Pichu",
-        correct: "C"
-    },
-    {
-        question: "What is the evolved form of Eevee that was introduced in Generation 2?",
-        imgSrc: "Assets/PNGs/Eevee.png",
-        choiceA: "Jolteon",
-        choiceB: "Espeon",
-        choiceC: "Vaporeon",
-        choiceD: "Leafeon",
-        correct: "B"
-    },
-    {
-        question: "Which Pokemon evolves into Steelix when traded while holding a Metal Coat?",
-        imgSrc: "Assets/PNGs/Steelix.png",
-        choiceA: "Onix",
-        choiceB: "Scyther",
-        choiceC: "Sneasel",
-        choiceD: "Murkrow",
-        correct: "A"
-    },
-    {
-        question: "Which electric type move is known for its ability to paralyze the target?",
-        imgSrc: "Assets/PNGs/Electric_Type_Icon.png",
-        choiceA: "Thunderbolt",
-        choiceB: "Thunder Wave",
-        choiceC: "Thunder Punch",
-        choiceD: "Thunder Shock",
-        correct: "B"
-    },
-    {
-        question: "Which Legendary Pokemon is known as the 'Master' of the Legenday Birds: Articuno, Zapdos, and Moltres? ",
-        imgSrc: "Assets/PNGs/Legendary_Birds.png",
-        choiceA: "Ho-Oh",
-        choiceB: "Mewtwo",
-        choiceC: "Lugia",
-        choiceD: "Mew",
-        correct: "C"
+#logo {
+    width: 100%;
+    max-height: 150px;
+    object-fit:scale-down;
+    position: absolute;
+    top: 0px;
+}
+#container{
+    margin : 150px auto;
+    background-color: #bdc4e1;
+    height: 300px;
+    width : 700px;
+    border-radius: 5px;
+    box-shadow: 0px 5px 15px 0px;
+    position: relative;
+    text-align: center;
+}
+
+#start{
+    font-size: 1.5em;
+    font-weight: bolder;
+    word-break: break-all;
+    width:180px;
+    height:40px;
+    border : 8px solid #5d5d5d;
+    text-align: center;
+    cursor: pointer;
+    position: absolute;
+    left:270px;
+    top:100px;
+    color : #000000;
+}
+
+#rules{
+    font-size: 18px;
+    margin-left: 285px;
+    text-align: left;
+    position: absolute;
+    bottom: 0;
+}
+    
+#rules ul {
+        list-style-type:circle;
+        padding: 0;
     }
-];
-
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let quizTime = 60;
-const gaugeWidth = 150;
-const gaugeUnit = gaugeWidth / quizTime;
-
-let TIMER;
-let score = 0;
-
-// render question
-function renderQuestion() {
-    let q = questions[runningQuestion];
-
-    question.innerHTML = "<p>" + q.question + "</p>";
-    qImg.innerHTML = "<img src=" + q.imgSrc + ">";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
-}
-
-start.addEventListener("click", startQuiz);
-
-// start the quiz
-function startQuiz() {
-    document.getElementById("rules").style.display = "none";
-    start.style.display = "none";
-    renderQuestion();
-    quiz.style.display = "block";
-    renderProgress();
-    renderCounter();
-    quizTime = 60; // Set quiz time
-    TIMER = setInterval(renderCounter, 1000);
-}
-
-function renderProgress() {
-    for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-        progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+    
+#rules li {
+        font-size: 12px;
+        margin-bottom: 5px;
     }
+
+#quiz {
+    display: none;
 }
 
-function renderCounter() {
-    if (quizTime >= 0) {
-        counter.innerHTML = quizTime;
-        timeGauge.style.width = quizTime * gaugeUnit + 'px';
-        quizTime--;
-    } else {
-        clearInterval(TIMER);
-        scoreRender();
-    }
+#start:hover{
+    border: 8px solid #ffdd02;
+    color : #263bb6;
 }
 
-// checkAnswer
-function checkAnswer(answer) {
-    if (answer == questions[runningQuestion].correct) {
-        // correct answer
-        score++;
-        // change progress color to green
-        answerIsCorrect();
-    } else {
-        // answer is wrong
-        // change progress color to red
-        answerIsWrong();
-    }
-    if (runningQuestion < lastQuestion) {
-        runningQuestion++;
-        renderQuestion();
-    } else {
-        // end the quiz and show the score
-        clearInterval(TIMER);
-        scoreRender();
-    }
+#qImg{
+    width : 200px;
+    height : 200px;
+    position: absolute;
+    left : 0px;
+    top : 0px;
+}
+#qImg img{
+    width : 200px;
+    height : 200px;
+    border-top-left-radius: 5px;
 }
 
-// answer is correct
-function answerIsCorrect() {
-    document.getElementById(runningQuestion).style.backgroundColor = "#3cb371";
+#question{
+    width:500px;
+    height : 125px;
+    position: absolute;
+    right:0;
+    top:0;
+}
+#question p{
+    margin : 0;
+    padding : 15px;
+    font-size: 1.25em;
 }
 
-// answer is Wrong
-function answerIsWrong() {
-    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+#choices{
+    width : 480px;
+    position: absolute;
+    right : 0;
+    top : 125px;
+    padding : 10px
+}
+.choice{
+    display: inline-block;
+    width : 135px;
+    text-align: center;
+    border : 1px solid #7e7e7e;
+    border-radius: 50px;
+    cursor: pointer;
+    padding : 5px;
+    margin: 5px;
+}
+.choice:hover{
+    border : 1px solid #0372cd;
+    background-color: #edd202
 }
 
-function saveScore() {
-    const initialsInput = document.getElementById("initials");
-    const userInitials = initialsInput.value.toUpperCase(); // Convert to uppercase
-    const userScore = Math.round(100 * score / questions.length);
+#timer{
+    position: absolute;
+    height : 75px;
+    width : 200px;
+    bottom : -40px;
+    text-align: center;
+    left:100;
+}
+#counter{
+    font-size: 50px;
+    margin-left: -390px;
 
-    if (userInitials && userScore) {
-        // Save the score in your preferred storage (localStorage, database, etc.)
-        // For example, save it in localStorage
-        const savedScores = JSON.parse(localStorage.getItem("savedScores")) || [];
-        savedScores.push({ initials: userInitials, score: userScore });
-        localStorage.setItem("savedScores", JSON.stringify(savedScores));
+}
+#btimeGauge{
+    width : 150px;
+    height : 10px;
+    border-radius: 10px;
+    background-color: #d4d4d4;
+    margin-left : -170px;
+}
+#timeGauge{
+    height : 10px;
+    border-radius: 10px;
+    background-color: #3cb371;
+    margin-top : -10px;
+    margin-left : -170px;
+}
+#progress{
+    width : 200px;
+    position: absolute;
+    bottom : 0px;
+    right : 235px;
+    padding: 5px;
+    text-align: right;
+}
+.prog{
+    width : 25px;
+    height : 25px;
+    border: 2px solid #000000;
+    display: inline-block;
+    border-radius: 40%;
+    margin-left : 5px;
+    margin-right : 5px;
+}
+#scoreContainer{
+    margin : 20px auto;
+    background-color: #bdc4e1;
+    height: 300px;
+    width : 700px;
+    border-radius: 5px;
+    box-shadow: 0px 5px 15px 0px;
+    position: relative;
+    display: none;
+    overflow: hidden;
+}
+#scoreContainer img{
+    width: 35%;
+    max-height: 35%;
+    object-fit: contain;
+    top: 40px;
+    left: 90px;
+    transform: translate(-50%);
+    position: absolute;
 
-        // Display the leaderboard
-        displayLeaderboard();
-    }
+}
+#scoreContainer p{
+    position: absolute;
+    display: block;
+    width : 59px;
+    height :59px;
+    top:60px;
+    right:500px;
+    font-size: 40px;
+    font-weight: bold;
+    text-align: center;
+    color:#000000
+}
+#leaderboard-container {
+    display: none;
+    position: absolute;
+    top: 0;
+    left: 100%; 
+    margin-left: 20px; 
+    width: 300px; 
+    background-color: #bdc4e1;
+    height: 300px;
+    border-radius: 5px;
+    box-shadow: 0px 5px 15px 0px;
+    overflow: hidden; 
 }
 
-function displayLeaderboard() {
-    const leaderboardContainer = document.getElementById("leaderboard-container");
-    const leaderboardList = document.getElementById("leaderboard");
-
-    // Clear previous leaderboard entries
-    leaderboardList.innerHTML = "";
-
-    // Retrieve saved scores from storage
-    const savedScores = JSON.parse(localStorage.getItem("savedScores")) || [];
-
-    // Sort scores in descending order
-    const sortedScores = savedScores.sort((a, b) => b.score - a.score);
-
-    // Display the top scores in the leaderboard
-    sortedScores.slice(0, 5).forEach((entry, index) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = `${index + 1}. ${entry.initials}: ${entry.score}%`;
-        leaderboardList.appendChild(listItem);
-    });
-
-    // Show the leaderboard container
-    leaderboardContainer.style.display = "block";
+#leaderboard-container h2 {
+    margin-top: 10px;
+    padding: 10px;
+    background-color: #5d5d5d;
+    color: #fff;
 }
 
-// score render
-function scoreRender() {
-    scoreDiv.style.display = "block";
+#leaderboard {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    overflow-y: auto; 
+    max-height: calc(100% - 40px); 
+}
 
-    // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score / questions.length);
+#leaderboard li {
+    font-size: 12px;
+    padding: 10px;
+    border-bottom: 1px solid #ddd;
+}
 
-    // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "Assets/PNGs/80_Correct.png" :
-        (scorePerCent >= 60) ? "Assets/PNGs/60_Correct.png" :
-            (scorePerCent >= 40) ? "Assets/PNGs/40_Correct.png" :
-                (scorePerCent >= 20) ? "Assets/PNGs/20_Correct.png" :
-                    "Assets/PNGs/0_Correct.png";
-
-    scoreDiv.innerHTML = "<img src=" + img + ">";
-    scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
-
-    // Add initials input and Save Score button
-    scoreDiv.innerHTML += "<div id='initials-container'>";
-    scoreDiv.innerHTML += "<label for='initials'>Enter your initials:</label>";
-    scoreDiv.innerHTML += "<input type='text' id='initials' maxlength='3' placeholder='AAA'>";
-    scoreDiv.innerHTML += "<button onclick='saveScore()'>Save Score</button>";
-    scoreDiv.innerHTML += "</div>";
-
-    // Add leaderboard container
-    scoreDiv.innerHTML += "<div id='leaderboard-container'>";
-    scoreDiv.innerHTML += "<h2>Leaderboard</h2>";
-    scoreDiv.innerHTML += "<ul id='leaderboard'></ul>";
-    scoreDiv.innerHTML += "</div>";
-
-    // Display the leaderboard
-    displayLeaderboard();
+#leaderboard li:last-child {
+    border-bottom: none;
 }
